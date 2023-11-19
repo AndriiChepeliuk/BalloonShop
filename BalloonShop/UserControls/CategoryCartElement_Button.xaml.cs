9f1f7763881a52f258de000cd50916aa1a033f47
Andrii <andrewchepeliuk@gmail.com>
2023-11-20 00:11:15 +0200

    В латексних кульках категорії не як кнопки

---
+++ BalloonShop/UserControls/CategoryCartElement_Button.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BalloonShop.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CategoryCartElement_Button.xaml
+    /// </summary>
+    public partial class CategoryCartElement_Button : UserControl
+    {
+        public CategoryCartElement_Button()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly RoutedEvent ElementClickEvent =
+            EventManager.RegisterRoutedEvent("ElementClick", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(CategoryCartElement_Button));
+
+        public event RoutedEventHandler ElementClick
+        {
+            add { AddHandler(ElementClickEvent, value); }
+            remove { RemoveHandler(ElementClickEvent, value); }
+        }
+
+        void Button_Click(object sender, RoutedEventArgs e) => RaiseEvent(new RoutedEventArgs(ElementClickEvent));
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(CategoryCartElement_Button));
+
+        public BitmapImage ImageBitmap
+        {
+            get { return (BitmapImage)GetValue(ImageBitmapProperty); }
+            set { SetValue(ImageBitmapProperty, value); }
+        }
+        public static readonly DependencyProperty ImageBitmapProperty = DependencyProperty.Register("ImageBitmap", typeof(BitmapImage), typeof(CategoryCartElement_Button));
+    }
+}
---
+++ BalloonShop/ViewModels/App_LatexBalloons_ViewModel.cs
@@ -0,0 +1,56 @@
+﻿using BalloonShop.Models.LatexBalloonType;
+using BalloonShop.Services;
+using BalloonShop.Views;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BalloonShop.ViewModels;
+
+public class App_LatexBalloons_ViewModel : ViewModelBase
+{
+    private MainWindow window;
+    private LatexBalloonTypeModel _selectedBalloobType;
+    private ObservableCollection<LatexBalloonTypeModel> _balloonTypes;
+
+    public LatexBalloonTypeModel SelectedBalloobType
+    {
+        get { return _selectedBalloobType; }
+        set
+        {
+            _selectedBalloobType = value;
+            OnPropertyChanged(nameof(SelectedBalloobType));
+        }
+    }
+    public ObservableCollection<LatexBalloonTypeModel> BalloonTypes
+    {
+        get { return _balloonTypes; }
+        set
+        {
+            _balloonTypes = value;
+            OnPropertyChanged(nameof(BalloonTypes));
+        }
+    }
+
+    public ICommand AddNewBalloonTypeCommand { get; }
+
+    public App_LatexBalloons_ViewModel()
+    {
+        BalloonTypes = new ObservableCollection<LatexBalloonTypeModel>(BalloonTypeModelService.GetAllBalloonTypes());
+        AddNewBalloonTypeCommand = new ViewModelCommand(ExecuteAddNewBalloonTypeCommand);
+    }
+
+    private void ExecuteAddNewBalloonTypeCommand(object obj)
+    {
+        LoadWindow();
+        window.ExecutePage(AppPages.LatexBalloons_WithImages_Page);
+    }
+
+    private void LoadWindow()
+    {
+        if (window == null)
+        {
+            window = Window.GetWindow(App.Current.MainWindow) as MainWindow;
+        }
+    }
+}
