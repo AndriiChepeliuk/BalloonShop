b2fb836ec4b8610c1f1592f1f5ab0ee2f61122d6
Andrii <andrewchepeliuk@gmail.com>
2023-12-03 18:14:20 +0200

    Додано макет відображення для латексних кульок

---
+++ BalloonShop/UserControls/LatexBalloonEntityElement.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace BalloonShop.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LatexBalloonEntityElement.xaml
+    /// </summary>
+    public partial class LatexBalloonEntityElement : UserControl
+    {
+        public LatexBalloonEntityElement()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("BalloonName", typeof(string), typeof(LatexBalloonEntityElement));
+
+        public BitmapImage ImageBitmap
+        {
+            get { return (BitmapImage)GetValue(ImageBitmapProperty); }
+            set { SetValue(ImageBitmapProperty, value); }
+        }
+        public static readonly DependencyProperty ImageBitmapProperty = DependencyProperty.Register("ImageBitmap", typeof(BitmapImage), typeof(LatexBalloonEntityElement));
+    }
+}
